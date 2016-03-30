e2dc90126acbea6323ecfbc2665bb797bf1aa39f
mitogh <mitogh@gmail.com>
2016-03-30 14:12:27 -0600

    Helper class to Sanitize values

---
+++ src/Sanitize.php
@@ -0,0 +1,23 @@
+<?php namespace Lean\Endpoints;
+
+/**
+ * Class that allw some helper functions to Sanitize elements.
+ *
+ * @package Lean\Endpoints
+ */
+class Sanitize {
+
+	/**
+	 * Function that iterates over an array to Sanitize the value of the array
+	 * in secure text values.
+	 *
+	 * @param array $data The data to be Sanitized.
+	 * @return array The cleaned data.
+	 */
+	public static function text_array( array $data = [] ) {
+		$map = function( $item ) {
+			return sanitize_text_field( $item );
+		};
+		return array_map( $map, $data );
+	}
+}
