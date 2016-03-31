21accd74747996d496558a93f07f71e3775e14c1
mitogh <mitogh@gmail.com>
2016-03-30 22:52:37 -0600

    Add filter used on this class

---
+++ src/Collection/Filter.php
@@ -0,0 +1,12 @@
+<?php namespace Lean\Endpoints\Collection;
+
+/**
+ * Custom filters used on this endpoint.
+ *
+ * @package Endpoints\Collection;
+ * @since 0.1.0
+ */
+class Filter {
+	const ITEM_IS_ENABLED = 'ln_collection_enable_filter_format';
+	const ITEM_FORMAT = 'ln_collection_item_format';
+}
