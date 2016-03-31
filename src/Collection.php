e71ce6ce746c093d3533a2484bf1b9132fd5e26e
mitogh <mitogh@gmail.com>
2016-03-30 18:18:07 -0600

    Fix CI issues

--- src/Collection.php
+++ src/Collection.php
@@ -14,16 +14,43 @@ class Collection extends AbstractEndpoint {
 	 * Path of the new endpoint.
 	 *
 	 * @Override
+	 *
+	 * @since 0.1.0
 	 * @var String
 	 */
 	protected $endpoint = '/collection';
+
+	/**
+	 * Array that holds all the shared arguments used for query the site.
+	 *
+	 * @var array
+	 * @since 0.1.0
+	 */
 	protected $args = [];
 
+	/**
+	 * Function inherint from the parant Abstract class that is called once the
+	 * endpoint has been initiated and the method that returns the data delivered
+	 * to the endpoint.
+	 *
+	 * @Override
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param \WP_REST_Request $request The request object that mimics the request
+	 *									made by the user.
+	 * @return array The data to be delivered to the endpoint
+	 */
 	public function endpoint_callback( \WP_REST_Request $request ) {
 		$this->args = $request->get_params();
 		return $this->filter_data( $this->loop() );
 	}
 
+	/**
+	 * WP_Query Loop that has been triggered from the endpoint.
+	 *
+	 * @return array An array with the data associated with the request.
+	 */
 	protected function loop() {
 		$data = [];
 		$custom_query = new \WP_Query( $this->args );
@@ -38,6 +65,16 @@ class Collection extends AbstractEndpoint {
 		return $data;
 	}
 
+	/**
+	 * Clean up and make sure we don't deliver post with password, privates and
+	 * some other datat that might be sensible on the API. This medhod overrides
+	 * the default mechanism inherint from the parent class.
+	 *
+	 * @Override
+	 *
+	 * @since 0.1.0
+	 * @return array an array with the accepted arguments and options per each argument.
+	 */
 	public function endpoint_args() {
 		return [
 			'post_type' => [
@@ -56,12 +93,8 @@ class Collection extends AbstractEndpoint {
 					return 'publish' === $post_status;
 				},
 			],
-			'has_password' => [
-				'validate_callback' => '__return_false',
-			],
-			'post_password' => [
-				'validate_callback' => '__return_false',
-			],
+			'has_password' => [ 'validate_callback' => '__return_false' ],
+			'post_password' => [ 'validate_callback' => '__return_false' ],
 		];
 	}
 }
